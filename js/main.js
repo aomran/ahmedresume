cf05685659fafa2a6318633a1fba0c77bb0f7b5c
Ahmed Omran <aomra015@gmail.com>
2013-06-27 11:36:48 -0400

    custom made work tab feature with jQuery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+	
+	//Add 'active' class to aside main nav corresponding to
+	//scroll position
 	$('body').scrollspy({ target: 'aside' });
-	$( "#tabs" ).tabs({ show: true });
+	
+	//Initialize tabs, show only first tab
+	$tabs = $('.work-tabs-body li')
+	$tabs.hide();
+	$tabs.first().show();
+
+	//Tabs on-click even handler
+	$tabHeader = $('.work-tabs-header li a')
+	$tabHeader.on("click", function(e){
+		e.preventDefault();
+		
+		//Find id of clicked tab from the href of tab-header
+		$desiredTab = $(this).attr("href");
+
+		//Hide visible tabs, show desired tab
+		$tabs.hide();
+		$($desiredTab).show("clip", 1000);
+	});
+
 });
